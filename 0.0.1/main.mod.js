81ef79ad3f9f3833af0a2b4d6389ab44c93705cb
Byzonyx <27mcleoal@stpatricks.qld.edu.au>
2025-10-10 21:38:04 +1100

    Corrected link to cdn

--- 0.0.1/main.mod.js
+++ 0.0.1/main.mod.js
@@ -1,9 +1,7 @@
-import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/0.5.1/PolyModLoader.js";
+import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
 
 class SpeedChecker extends PolyMod {
-    onLoad() {
-        console.log("Speed Checker mod loaded!");
-    }
+
 }
 
 export let polyMod = new SpeedChecker();
