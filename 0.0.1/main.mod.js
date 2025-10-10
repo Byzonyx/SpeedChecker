e45230805b979b617c0a2a8925938f3e3c9433f9
Byzonyx <27mcleoal@stpatricks.qld.edu.au>
2025-10-10 21:58:32 +1100

    pls work

--- 0.0.1/main.mod.js
+++ 0.0.1/main.mod.js
@@ -1,18 +1,7 @@
 import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
 
 class SpeedChecker extends PolyMod {
-    constructor() {
-        super();
-        this.name = "Speed Checker";
-    }
 
-    onLoad() {
-        // Minimal code to ensure the mod loads
-    }
-
-    onUnload() {
-        // Clean up if needed
-    }
 }
 
 export let polyMod = new SpeedChecker();
