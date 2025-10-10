4821549550715fbbc2390683c602277d0bf3a940
Byzonyx <27mcleoal@stpatricks.qld.edu.au>
2025-10-10 22:03:22 +1100

    NOOO its not working anymore

--- 0.0.1/main.mod.js
+++ 0.0.1/main.mod.js
@@ -1,7 +1,7 @@
 import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
-
 class SpeedChecker extends PolyMod {
-
+    
 }
+
 
 export let polyMod = new SpeedChecker();
